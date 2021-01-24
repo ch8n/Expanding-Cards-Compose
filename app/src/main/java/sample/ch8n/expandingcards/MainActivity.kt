d358fb5b70e52d90d5379515ad0aff5451719ea8
ch8n <chetan.garg36@gmail.com>
2021-01-24 20:20:02 +0530

    fixed/deps

--- app/src/main/java/sample/ch8n/expandingcards/MainActivity.kt
+++ app/src/main/java/sample/ch8n/expandingcards/MainActivity.kt
@@ -2,21 +2,18 @@ package sample.ch8n.expandingcards
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.Text
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import sample.ch8n.expandingcards.component.CardComponent
@@ -42,43 +39,17 @@ class MainActivity : AppCompatActivity() {
 fun Greeting(name: String) {
     Column(modifier = Modifier.fillMaxSize()) {
         Text(text = "Hello $name!")
-        var expandedPositionState by remember { mutableStateOf(1) }
 
-        Row(modifier = Modifier.fillMaxWidth(1f).background(Color.Blue)) {
 
-            CardComponent(
-                modifier = Modifier.fillMaxWidth(
-                    if (expandedPositionState == 1) {
-                        0.5f
-                    } else {
-                        0.25f
-                    }
-                ).clickable(onClick = {
-                    expandedPositionState = 1
-                })
-            )
-            CardComponent(
-                modifier = Modifier.fillMaxWidth(
-                    if (expandedPositionState == 2) {
-                        0.5f
-                    } else {
-                        0.25f
-                    }
-                ).clickable(onClick = {
-                    expandedPositionState = 2
-                })
-            )
-            CardComponent(
-                modifier = Modifier.fillMaxWidth(
-                    if (expandedPositionState == 3) {
-                        0.5f
-                    } else {
-                        0.25f
-                    }
-                ).clickable(onClick = {
-                    expandedPositionState = 3
-                })
-            )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Blue)
+        ) {
+
+            CardComponent()
+            CardComponent()
+            CardComponent()
         }
 
     }
--- app/src/main/java/sample/ch8n/expandingcards/component/card.kt
+++ app/src/main/java/sample/ch8n/expandingcards/component/card.kt
@@ -2,56 +2,68 @@ package sample.ch8n.expandingcards.component
 
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.Text
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageAsset
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import sample.ch8n.expandingcards.R
 
+@Preview(showBackground = true, widthDp = 400, heightDp = 300)
 @Composable
-fun CardComponent(modifier: Modifier) {
-    Box(modifier = modifier) {
+fun CardComponent() {
 
-        Card(
-            modifier = Modifier.fillMaxWidth().padding(8.dp),
-            elevation = 8.dp
+    var isExpanded by remember { mutableStateOf(false) }
+
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .clickable(onClick = {
+                isExpanded = !isExpanded
+            }),
+        elevation = 8.dp
+    ) {
+
+        Box(
+            modifier = Modifier.fillMaxWidth().height(200.dp)
         ) {
 
-            Box(
-                modifier = Modifier.fillMaxWidth().height(200.dp)
-            ) {
+            val (userName, imageUrl) = getRandomSample()
 
-                val (userName, imageUrl) = getRandomSample()
-
-                loadImage(
-                    url = imageUrl,
-                    placeholder = R.drawable.ic_launcher_background
-                ).value?.let {
-                    Image(
-                        modifier = Modifier.fillMaxSize(),
-                        asset = it.asImageAsset(),
-                        contentScale = ContentScale.Crop
-                    )
-                }
-
-                Text(
-                    text = userName,
-                    color = Color.White,
-                    modifier = Modifier.align(Alignment.BottomEnd)
-                        .padding(8.dp)
+            loadImage(
+                url = imageUrl,
+                placeholder = R.drawable.ic_launcher_background
+            ).value?.let {
+                Image(
+                    modifier = Modifier.fillMaxSize(),
+                    bitmap = it.asImageBitmap(),
+                    contentScale = ContentScale.Crop
                 )
-
             }
 
+            Text(
+                text = userName,
+                color = Color.White,
+                modifier = Modifier.align(Alignment.BottomEnd)
+                    .padding(8.dp)
+            )
 
         }
+
+
     }
+
 
 }
